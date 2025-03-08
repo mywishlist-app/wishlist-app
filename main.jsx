b73573086cb278781ec0626898b849111f87adcb
mywishlist-app <gokuraku.mangetsu.shisha@gmail.com>
2025-03-08 13:33:24 +0900

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WishlistApp from "./WishlistApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WishlistApp />
+  </React.StrictMode>
+);
